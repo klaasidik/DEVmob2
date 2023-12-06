67bfebcc26f6babe873fbae2f8888fd2316a2096
SIDIK KLAA <Klaasidik@gmail.com>
2023-12-05 19:14:21 -0500

    first commit

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -89,12 +89,24 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} """
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dashboard',
+        'USER': 'root',
+        'PASSWORD': '40902019',
+        'HOST': 'localhost',   # Ou une adresse IP/hostname
+        'PORT': '3306',        # Port par défaut de MySQL
+    }
 }
+
+
 AUTH_USER_MODEL = 'activityTracker.User'
 
 # Password validation
